4588fdc39c74ab6753d66b01f79367fda9520dc0
Aditya  Singh <aadityasinghh2360@gmail.com>
2026-04-26 23:09:43 +0530

    lecture 2

--- springboot/lecture1/DependancyInjection.java
+++ springboot/lecture1/DependancyInjection.java
@@ -13,5 +13,27 @@ Types of Dependany injection
 
 Web FrameWork ->  A web framework in Java is a collection of pre-written code, libraries, and components designed to simplify the development of web applications, services, and APIs
     
+Problems Without a Framework
+If you build from scratch, you must manually handle every technical detail, including:
+1. Request Management: Manually handling incoming HTTP requests and parsing query parameters.
+2 .Response Control: Managing status codes and headers yourself.
+3 .User Tracking: Maintaining sessions or cookies to keep users logged in.
+4 .Operations: Writing your own code for error handling, logging, and database connections.
+
+Benefits of Using a Framework
+
+A framework acts as a shortcut by providing these advantages:
+1. Less Repetition: Significantly reduces "boilerplate" code (tedious, repetitive code).
+2.Better Organization: Provides a structured architecture so the code stays neat.
+3 .Automation: Handles all the common technical tasks (like the problems listed above) automatically.
+4 .Quality: Makes your application easier to maintain, test, and grow (scalable).
+Would you like to see a comparison of how a single task (like database connection) looks with vs. without a framework?
+AI can make mistakes, so double-check responses
+
+
+
+
+
+
     */
 }
---
+++ springboot/lecture2/springconcepts/SpringLec1.java
@@ -0,0 +1,44 @@
+package springboot.lecture2.springconcepts;
+
+/*
+
+1. Bean ->   Spring Bean is an object. However, it is an object that is managed by the Spring framework, rather than being managed by you.
+
+ 
+Spring Container (IoC Container)
+The container is the brain of Spring. It's responsible for:
+->Creating all beans
+->Injecting their dependencies
+->Managing their lifecycle
+
+Inversion of Control (IoC)
+Normally, your code controls when and how objects are created. With Spring, you give up that control — Spring takes over and creates objects for you.
+ApplicationContext
+This is the main Spring container implementation. It knows every bean, manages them, and resolves dependencies.
+
+
+Build Tool
+
+1. Maven  ->  Maven ek Java tool hai jo Spring projects mein build aur dependency management ko easy banata hai. Simple shabdon mein, Maven ek helper hai jo tumhare project ke libraries (dependencies) ko automatically download karta hai aur project ko compile, test, aur package karne ka pura process automate karta hai.
+
+
+=> Moving tO Anotation
+a. Tedious
+b. Hard to add Beans
+c. Bioler Plate  Code
+d. Error Prone
+
+Anotation is Special Marker Start with @ 
+
+
+
+
+
+
+
+*/
+public class SpringLec1 {
+    public static void main(String[] args) {
+        
+    }
+}
